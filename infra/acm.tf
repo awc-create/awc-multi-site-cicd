c7587ceb9cc5e5a526586fb0c4690a499ea2972e
Adnan Said <adnansaid@Adnans-MacBook-Air.local>
2025-04-12 05:01:51 +0100

    chore: trigger deploy for main

--- infra/acm.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_acm_certificate" "ssl_cert" {
-  count             = var.domain_name == "" ? 0 : 1
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,16 +1,4 @@
 variable "project_name" {
-  description = "The GitHub repo name used to generate unique resource names"
   type        = string
-}
-
-variable "domain_name" {
-  description = "The custom domain name (optional)"
-  type        = string
-  default     = ""
-}
-
-variable "zone_id" {
-  description = "Route53 Hosted Zone ID (optional)"
-  type        = string
-  default     = ""
+  description = "Unique name for project (used in bucket, certs, etc.)"
 }
